edca5f3e7257f2f0e5987e4ded313f8edf7f4bca
ottojo <jottosmail@gmail.com>
2016-05-09 21:51:32 +0200

    added buttons to continue after game end

--- TicTacToe/app/src/main/java/com/jonasotto/tictactoe/SpielEndeActivity.java
+++ TicTacToe/app/src/main/java/com/jonasotto/tictactoe/SpielEndeActivity.java
@@ -1,9 +1,10 @@
 package com.jonasotto.tictactoe;
 
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.res.ResourcesCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class SpielEndeActivity extends AppCompatActivity {
@@ -33,5 +34,17 @@ public class SpielEndeActivity extends AppCompatActivity {
                 textViewGewinner.setTextColor(ContextCompat.getColor(this, R.color.spieler2));
                 break;
         }
+    }
+
+    public void hauptmenue(View view) {
+        Intent spielEnde = new Intent(SpielEndeActivity.this, MainActivity.class); //Neuer Intent um MainActivity zu starten
+        startActivity(spielEnde);   //Intent ausführen, neue Activity MainActivity starten
+        finish();   //Aktuelle Activity beenden
+    }
+
+    public void neuesSpiel(View view) {
+        Intent spielEnde = new Intent(SpielEndeActivity.this, SpielActivity.class); //Neuer Intent um SpielActivity zu starten
+        startActivity(spielEnde);   //Intent ausführen, neue SpielActivity starten
+        finish();   //Aktuelle Activity beenden
     }
 }
