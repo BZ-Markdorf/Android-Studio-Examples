a1f31a007a4dea7f6cb954b832e0a577d4448577
ottojo <jottosmail@gmail.com>
2016-05-25 02:14:55 +0200

    fixed board handling

--- TicTacToe/app/src/main/java/com/jonasotto/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/jonasotto/tictactoe/MainActivity.java
@@ -1,8 +1,8 @@
 package com.jonasotto.tictactoe;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,8 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void spielStarten(View view)
-    {
+    public void spielStarten(View view) {
         Intent spielStart = new Intent(MainActivity.this, SpielActivity.class); //Neuer Intent um SpielActivity zu starten
         startActivity(spielStart);   //Intent ausführen, neue Activity SpielActivity starten
         finish();   //Aktuelle Activity beenden
